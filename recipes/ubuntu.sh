1862f75da6e498de5e026c26333d9b962321a3b5
Brandon Toone <xsonih@gmail.com>
2012-01-03 22:54:28 -0700

    Removes call to upgrade OS
    Upgrading the OS wipes out any shared folders on the virtual machine

--- recipes/ubuntu.sh
+++ recipes/ubuntu.sh
@@ -32,8 +32,7 @@ echo -e "\nUsing $pm for package installation\n"
 
 # Update the system before going any further
 echo -e "\n=> Updating system (this may take a while)..."
-sudo $pm update >> $log_file 2>&1 \
- && sudo $pm -y upgrade >> $log_file 2>&1
+sudo $pm update >> $log_file 2>&1
 echo "==> done..."
 
 # Install build tools
